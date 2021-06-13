8722eef0dffc81af181c4fe16bdd184c89b391c0
Gata <changatasan@gmail.com>
2021-06-13 23:06:35 +0400

    Цезарь нарезан и приготовлен

--- src/com/company/DeShifr.java
+++ src/com/company/DeShifr.java
@@ -6,16 +6,17 @@ import java.util.Scanner;
 
     public class DeShifr {
 
-        public void iterations(char[] ch){
+        public void iterations(String ch){
             char[] massive = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".toCharArray();
+            char[] array = ch.toLowerCase(Locale.ROOT).toCharArray();
             for (int i = 0; i < 33; i++) {
                 System.out.println("\n\nСдвиг = " + i );
-                for (int j = 0; j < ch.length; j++) {
-                    if ((ch[j] == ' ') || (ch[j] == '\n') || (ch[j] == '.') || (ch[j] == ',')) {
-                        System.out.print(ch[j]);
+                for (int j = 0; j < array.length; j++) {
+                    if ((array[j] == ' ') || (array[j] == '\n') || (array[j] == '.') || (array[j] == ',')) {
+                        System.out.print(array[j]);
                     }
                     for (int k = 0; k < massive.length; k++){
-                        if (ch[j] == massive[k]){
+                        if (array[j] == massive[k]){
                             if (k + i < 33) {
                                 System.out.print(massive[k + i]);
                             } else {
